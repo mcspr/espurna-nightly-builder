3ab97427ea860be230abbe24240d9fa7e2cf3302
Max Prokhorov <prokhorov.max@outlook.com>
2018-06-19 15:18:58 +0300

    Use prebuilt webui, for now

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-
+set -e
 pushd espurna/code
-./build.sh ${1}
-find ../firmware/
+
+ENVIRONMENT=${1}
+APP_REVISION=$(git rev-parse --short HEAD)
+APP_VERSION=$(grep APP_VERSION espurna/config/version.h | awk '{print $3}' | sed 's/"//g')
+OUTPUT_DIR="../firmware/espurna-${APP_VERSION}"
+
+cp espurna/config/version.h espurna/config/version.h.original
+sed -i -e "s/APP_REVISION            \".*\"/APP_REVISION            \"$APP_REVISION\"/g" espurna/config/version.h
+
+platformio run -s -e ${ENVIRONMENT}
+
+mkdir -p ${OUTPUT_DIR}
+cp .pioenvs/${1}/firmware.bin ${OUTPUT_DIR}/espurna-${APP_VERSION}.git${APP_REVISION}-${ENVIRONMENT}.bin
+
 popd
 
