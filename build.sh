ad5a32af29dd558c22661ee56c895de5f9d51eb4
Max Prokhorov <prokhorov.max@outlook.com>
2018-06-26 08:27:57 +0300

    Test stage, conditional

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ OUTPUT_DIR="../firmware/espurna-${APP_VERSION}"
 cp espurna/config/version.h espurna/config/version.h.original
 sed -i -e "s/APP_REVISION            \".*\"/APP_REVISION            \"$APP_REVISION\"/g" espurna/config/version.h
 
-platformio run -s -e ${ENVIRONMENT}
+time node node_modules/gulp/bin/gulp.js
+time platformio run -s -e ${ENVIRONMENT}
 
 mkdir -p ${OUTPUT_DIR}
 cp .pioenvs/${1}/firmware.bin ${OUTPUT_DIR}/espurna-${APP_VERSION}.git${APP_REVISION}-${ENVIRONMENT}.bin
