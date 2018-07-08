5ebc98965f55527706882505d8951142f0b8ccd3
Max Prokhorov <prokhorov.max@outlook.com>
2018-07-08 19:21:44 +0300

    Make build script aware of BUILDER_THREAD variables

--- build.sh
+++ build.sh
@@ -2,13 +2,27 @@
 set -e
 pushd espurna/code
 
-ENVIRONMENT=${1}
+# Reuse parallel build implementation from https://github.com/xoseperez/espurna/pull/986 (kudos to @lobradov)
+par_thread=${BUILDER_THREAD:-0}
+par_total_threads=${BUILDER_TOTAL_THREADS:-4}
+if [ ${par_thread} -ne ${par_thread} -o \
+    ${par_total_threads} -ne ${par_total_threads} ]; then
+    echo "Parallel threads should be a number."
+    exit
+fi
+if [ ${par_thread} -ge ${par_total_threads} ]; then
+    echo "Current thread is greater than total threads. Doesn't make sense"
+    exit
+fi
+
+environments=$(grep env: platformio.ini | grep -v ota  | grep -v ssl  | grep -v travis | sed 's/\[env://' | sed 's/\]/ /' | sort)
+environments=$(echo ${environments} | \
+    awk -v par_thread=${par_thread} -v par_total_threads=${par_total_threads} \
+    '{ for (i = 1; i <= NF; i++) if (++j % par_total_threads == par_thread ) print $i; }')
+
 APP_REVISION=$(git rev-parse --short HEAD)
 APP_VERSION=$(grep APP_VERSION espurna/config/version.h | awk '{print $3}' | sed 's/"//g')
 OUTPUT_DIR="../firmware/espurna-${APP_VERSION}"
-
-cp espurna/config/version.h espurna/config/version.h.original
-sed -i -e "s/APP_REVISION            \".*\"/APP_REVISION            \"$APP_REVISION\"/g" espurna/config/version.h
 
 time node node_modules/gulp/bin/gulp.js
 time platformio run -s -e ${ENVIRONMENT}
