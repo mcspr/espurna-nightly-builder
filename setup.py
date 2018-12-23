98bc1ad68744370cf27dddac5ebfe4c06741af11
Maxim Prokhorov <prokhorov.max@outlook.com>
2018-12-23 21:12:49 +0300

    pio dependency for requests

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name="espurna-nightly-builder",
       author="Maxim Prokhorov",
       license="License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
       install_requires=[
-          "requests"
+          "requests>=2.4.0,<3"
       ],
       packages=["espurna_nightly_builder"],
       test_suite="tests",
