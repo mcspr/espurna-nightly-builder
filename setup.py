c590987766bcf4ba0aa0caf7f862e30044ac2858
Maxim Prokhorov <prokhorov.max@outlook.com>
2021-06-05 10:45:01 +0300

    Migrate to setup.cfg

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
-from setuptools import setup
-setup(name="espurna-nightly-builder",
-      version="1.0",
-      description="ESPurna nightly builder utility",
-      author="Maxim Prokhorov",
-      license="License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-      install_requires=[
-          "requests>=2.4.0,<3"
-      ],
-      packages=["espurna_nightly_builder"],
-      entry_points={
-          "console_scripts": [
-              "espurna_nightly = espurna_nightly_builder.__main__:main"
-          ]
-      }
-)
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
